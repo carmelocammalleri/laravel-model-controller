46b113058b92264cf3c8a553afd9ebedefce800b
Melozzo <carmecamma1995@gmail.com>
2023-11-20 18:09:35 +0100

    [linked]: database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,16 @@
 
 @section("content")
     <h1>titolo film</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ea sunt laudantium officia, et unde illum eum? Perspiciatis ut, repudiandae ipsam quaerat sint, natus exercitationem repellat officia molestiae unde fugit?</p>
+    <div class="container d-flex flex-wrap">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="card-link">Card link</a>
+              <a href="#" class="card-link">Another link</a>
+            </div>
+          </div>
+    </div>
+
 @endsection
