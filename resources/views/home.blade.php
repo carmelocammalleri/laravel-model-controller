24b48a9b315d0a9417ec27102edbc940da5014e7
Melozzo <carmecamma1995@gmail.com>
2023-11-20 18:21:21 +0100

    [print]: element of database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,22 @@
 @section("content")
     <h1>titolo film</h1>
     <div class="container d-flex flex-wrap">
+
+        @foreach ($movies as $movie )
+
         <div class="card" style="width: 18rem;">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
+              <h5 class="card-title">{{ $movie->title }}</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie->original_title }}</h6>
+              <p class="card-text">Nationality: {{ $movie->nationality }} </p>
+              <p class="card-text">Vote: {{ $movie->vote }}</p>
+              <p class="card-text">Date: {{ $movie->date }}</p>
+
             </div>
-          </div>
+        </div>
+
+        @endforeach
+
     </div>
 
 @endsection
